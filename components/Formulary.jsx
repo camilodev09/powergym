0dd47609aa2d9e4810cfa9155ace4f8b31f78b3e
camilodev09 <camilobau2021@gmail.com>
2024-02-15 17:20:23 -0500

    xdc

--- components/Formulary.jsx
+++ components/Formulary.jsx
@@ -59,10 +59,10 @@ const Formulary = () => {
   };
 
   return (
-    <div className=" w-[95%] md:w-full  gap-4 p-4 rounded-lg items-center justify-center">
+    <div className="  gap-4 p-4 rounded-lg items-center justify-center">
       <form
         onSubmit={handleSend}
-        className="w-[95%] flex flex-col gap-3 lgl:gap-6 py-2 lgl:py-5 bg-white p-4 rounded-md shadow-todoShodow pt-8 md:pt-0 shadow-shadowOne"
+        className=" flex flex-col gap-3 lgl:gap-6 py-2 lgl:py-5 bg-white p-4 rounded-md shadow-todoShodow pt-8 md:pt-0 shadow-shadowOne"
       >
         <h3 className=" pb-4 text-center font-titleFont font-semibold text-gray-800">
           ¡Contáctanos adquiere los mejores planes!
@@ -95,7 +95,7 @@ const Formulary = () => {
               required
             />
           </div>
-          <div className="w-[48%] lgl:w-1/2 flex flex-col gap-1">
+          <div className="w-[45%] lgl:w-1/2 flex flex-col gap-1">
             <label
               className="text-sm text-gray-900  tracking-wide"
               htmlFor="lastname"
