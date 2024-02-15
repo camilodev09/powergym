fa6a73f1a1cd7439a04d4087c089ec063ed406e4
camilodev09 <camilobau2021@gmail.com>
2024-02-15 17:06:04 -0500

    xdfc

--- components/Form.jsx
+++ components/Form.jsx
@@ -2,7 +2,7 @@ import Formulary from "./Formulary";
 
 const Form = () => {
   return (
-    <div className="px-20   justify-center items-center  relative  top-44  flex md:hidden">
+    <div className="  justify-center items-center  relative  top-44  flex md:hidden">
       <div className="    flex  absolute  left-2 ">
         <Formulary />
       </div>
